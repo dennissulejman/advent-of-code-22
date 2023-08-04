a7ff6b04db4143b05d797955c86153a4e9a9bfdb
Dennis Sulejman <dennis_s93@hotmail.com>
2023-08-04 17:20:09 +0200

    Write Node.js-implementation

--- 1/node-js/app.js
+++ 1/node-js/app.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+import { once } from "events";
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
+(async function processLineByLine() {
+    const fileReadStream = createInterface({
+      input: createReadStream("..\\input.txt"),
+      crlfDelay: Infinity
+    });
+
+    let highestAmountOfCaloriesCarriedByElf = 0;
+    let amountOfCaloriesCarriedByCurrentElf = 0;
+
+    fileReadStream.on("line", (currentLine) => {
+      if (currentLine) {
+        amountOfCaloriesCarriedByCurrentElf += parseInt(currentLine);
+      } else {
+        highestAmountOfCaloriesCarriedByElf =
+        amountOfCaloriesCarriedByCurrentElf > highestAmountOfCaloriesCarriedByElf
+        ? amountOfCaloriesCarriedByCurrentElf
+        : highestAmountOfCaloriesCarriedByElf;
+
+        amountOfCaloriesCarriedByCurrentElf = 0;
+      }
+    });
+
+    await once(fileReadStream, "close");
+
+    console.log(highestAmountOfCaloriesCarriedByElf);
+})();
