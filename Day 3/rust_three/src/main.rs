c3dd33277a8eade62b43e2d0183be8162312b65f
Dennis Sulejman <dennis_s93@hotmail.com>
2023-10-24 22:21:09 +0200

    Implementations in Rust for Day 3 part 2

--- Day 3/rust_three/src/main.rs
+++ Day 3/rust_three/src/main.rs
@@ -7,13 +7,19 @@ use std::{
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 fn main() {
+    // Part 1
     let mut items_priority_sums = 0;
+
+    // Part 2
+    let mut groups_priority_sums = 0;
+    let mut group_items = vec![];
 
     let filename = "../Input.txt";
 
     if let Ok(lines) = read_lines(filename) {
         for line_result in lines {
             if let Ok(line) = line_result {
+                // Part 1
                 let full_len = line.len();
                 let half_len = full_len / 2;
 
@@ -30,11 +36,31 @@ fn main() {
                         break;
                     }
                 }
+
+                // Part 2
+                group_items.push(line);
+
+                if group_items.len() == 3 {
+                    for (_, c) in group_items[0].chars().enumerate() {
+                        if group_items[1].contains(c) && group_items[2].contains(c) {
+                            // Add one at the end because the priority starts at 1
+                            let priority = ALPHABET.find(c).expect("Not found!") + 1;
+                            groups_priority_sums += priority;
+
+                            // Only get the priority of the first match
+                            break;
+                        }
+                    }
+
+                    group_items.clear();
+                }
             }
         }
     }
-
+    // Part 1
     println!("{items_priority_sums}");
+    // Part 2
+    println!("{groups_priority_sums}");
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Day 1, Part 2: 204639 (C#, Go, Rust)
 Day 2, Part 1: 15523 (C#, Go, Rust)
 <br>
 Day 2, Part 2: 15702 (Rust)
+<br>
+Day 3, Part 1: 8515 (Rust)
+<br>
+Day 3, Part 2: 2434 (Rust)
 </details>
